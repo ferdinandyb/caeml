f6ada904a618293cf55d20d635d32aa97d32ce14
Koni Marti <koni.marti@gmail.com>
2022-11-07 08:21:54 +0100

    caeml: add -H flag to specify headers
    Add -H flag to specify headers as a comma-separated list. The order of
    the header values is determined by the order in the list. Any header
    value can be displayed. For example,
    
    caeml -H "From,To,Cc,Bcc,Date,Subject"
    
    corresponds to the default case if caeml is called without any
    arguments. However, we could also do the following now:
    
    caeml -H "Subject,To,From,Message-Id"
    
    Signed-off-by: Koni Marti <koni.marti@gmail.com>

--- caeml.go
+++ caeml.go
@@ -3,19 +3,40 @@ package main
 import (
 	"bufio"
 	"fmt"
-	_ "github.com/emersion/go-message/charset"
-	"github.com/emersion/go-message/mail"
 	"io"
 	"mime"
 	"os"
 	"strings"
+
+	"git.sr.ht/~sircmpwn/getopt"
+	_ "github.com/emersion/go-message/charset"
+	"github.com/emersion/go-message/mail"
 )
 
 func main() {
 	var r *mail.Reader
 
-	if len(os.Args) > 1 {
-		path := os.Args[1]
+	args := make([]string, len(os.Args))
+	copy(args, os.Args)
+
+	opts, optind, err := getopt.Getopts(args, "H:")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	headers := []string{"From", "To", "Cc", "Bcc", "Date", "Subject"}
+	for _, opt := range opts {
+		if opt.Option == 'H' {
+			headers = strings.Split(opt.Value, ",")
+			for i, _ := range headers {
+				headers[i] = strings.TrimSpace(headers[i])
+			}
+		}
+	}
+
+	if len(args[optind:]) > 0 {
+		path := strings.Join(args[optind:], " ")
 		f, err := os.Open(path)
 		if err != nil {
 			fmt.Println(err)
@@ -38,43 +59,55 @@ func main() {
 			os.Exit(1)
 		}
 	}
-	h := &mail.Header{Header: r.Header.Header}
-	fields := [4]string{"From", "To", "Cc", "Bcc"}
-	for _, field := range fields {
-		addrlist := []string{}
-		header, err := h.AddressList(field)
-		if err != nil {
-			continue
-		}
-		for _, address := range header {
 
-			dec := new(mime.WordDecoder)
-			name, err := dec.DecodeHeader(address.Name)
+	h := &mail.Header{Header: r.Header.Header}
+
+	for _, header := range headers {
+		switch strings.ToLower(header) {
+		case "from", "to", "cc", "bcc":
+			addrlist := []string{}
+			headerContent, err := h.AddressList(header)
 			if err != nil {
 				continue
 			}
-			addrlist = append(addrlist, name+" "+"<"+address.Address+">")
-		}
-		if len(addrlist) > 0 {
-			fmt.Print(field + ": ")
-			for i := 0; i < len(addrlist); i++ {
-				if i == len(addrlist)-1 {
-					fmt.Print(addrlist[i] + "\n")
-				} else {
-					fmt.Print(addrlist[i] + ", ")
+			for _, address := range headerContent {
+				dec := new(mime.WordDecoder)
+				name, err := dec.DecodeHeader(address.Name)
+				if err != nil {
+					continue
+				}
+				addrlist = append(addrlist, name+" "+"<"+address.Address+">")
+			}
+			if len(addrlist) > 0 {
+				fmt.Print(header + ": ")
+				for i := 0; i < len(addrlist); i++ {
+					if i == len(addrlist)-1 {
+						fmt.Print(addrlist[i] + "\n")
+					} else {
+						fmt.Print(addrlist[i] + ", ")
+					}
 				}
 			}
+		case "date":
+			time, err := h.Date()
+			if err == nil {
+				fmt.Println("Date: ", time)
+			}
+		case "subject":
+			subject, err := h.Subject()
+			if err == nil {
+				fmt.Println("Subject: ", subject)
+			}
+		default:
+			text, err := h.Text(header)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Println(header + ": " + text)
 		}
 	}
-	time, err := h.Date()
-	if err == nil {
-		fmt.Println("Date: ", time)
-	}
-	subject, err := h.Subject()
-	if err == nil {
 
-		fmt.Println("Subject: ", subject)
-	}
 	fmt.Print("\n--------------------------------------------------------\n\n")
 	for true {
 		part, err := r.NextPart()
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require github.com/emersion/go-message v0.16.0
 
 require (
+	git.sr.ht/~sircmpwn/getopt v1.0.0 // indirect
 	github.com/emersion/go-textwrapper v0.0.0-20200911093747-65d896831594 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
